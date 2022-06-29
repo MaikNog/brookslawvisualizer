10cda91a74cf780d3f586825b086afdd92bc57d8
MaikNog <maiknogens@gmail.com>
2022-06-29 21:38:08 +0200

    Removed printf debug lines, inserted team_list code

--- firsttry.py
+++ firsttry.py
@@ -1,79 +1,27 @@
-"""
-n*(n-1)/2
-
-Get input variable "team member"
-Save input to variable "team member"
-Calculate "lines of communication"
-Save calc result to variable "lines of communication"
-
-how to build up all possible connections?
-
-erste serie ist 6-1 wiederholt (1,1,1,1,1)
-Folgeserien sind 6-2, dann 6-3, dann 6-4, dann 6-5
-oder: team_size – iteration (faengt bei 1 an)
-
-Nummer=1
-while iteration < team_size ..
-    do repeat iteration (team_size – iteration)
-print iteration
-iteration+
-"""
-
-
-"""
-6 -> 15
-7 -> 21
-
-*5	10		*6	15
-
-1		2
-1		3
-1		4
-1		5
-1		6
-			2		3
-			2		4
-			2		5
-			2		6
-				3		4
-					3		5
-					3		6
-						4		5
-						4		6
-							5		6
-"""
-
-#team_size = 6
 team_size = 3
 iteration = 1
 
 total_list_count = 0
 total_list = []
+team_list = []
 
-#print("team_size", "number", "repetition")
 print("team_size:", team_size)
-print(" ")
+# Create list with elements from team_size in reverse order (range syntax)
+# e.g. team_size=4 has list (4, 3, 2, 1)
+for i in range(team_size, 0, -1):
+    team_list.append(int(i))
+print("team list :", team_list)
+
 while iteration < team_size:
     repetition = team_size - iteration
     new_list = [iteration] * repetition
 
-    #print(team_size, iteration, repetition)
-    #print("iteration, repetition :", iteration, repetition, " ==> ", "new_list :", new_list)
-
-    #print(len(new_list), "==>", new_list)
-    #print(new_list)
-
     total_list_count += len(new_list)
-    #print(total_list_count)
-
     total_list += new_list
-
     iteration += 1
-print(" ")
 print("total_list :", total_list)
 
-# create a GV file from the total_list
-# in current directory
+# create a GV file from the total_list in current directory
 fp = open('input.gv', 'w')
 
 # Opening definition and bracket of file
@@ -81,7 +29,6 @@ fp.write('graph MyGraph {')
 fp.write('\n')
 
 # Content from total_list
-
 for items in total_list:
     fp.writelines(str(items) + " -- " + "2" +'\n')
 

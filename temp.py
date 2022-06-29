d668ecf9f419a7c6e6368a7b881069cb9523b3ba
MaikNog <maiknogens@gmail.com>
2022-06-29 21:54:50 +0200

    Added temp.py for experimenting, focus on one code block, one problem. Here i write down the compare logic to iterate through to link the nodes to each other.

--- temp.py
+++ temp.py
@@ -1,47 +1,70 @@
-"""
-n*(n-1)/2
-
-erste serie ist 6-1 wiederholt (1,1,1,1,1)
-Folgeserien sind 6-2, dann 6-3, dann 6-4, dann 6-5
-oder: team_size – iteration (faengt bei 1 an)
-
-"""
-team_size = 5
+team_size = 3
 iteration = 1
 
 total_list_count = 0
 total_list = []
+team_list = []
 
+print("team_size:", team_size)
 # Create list with elements from team_size in reverse order (range syntax)
 # e.g. team_size=4 has list (4, 3, 2, 1)
-
-print("team list")
-team_list = []
 for i in range(team_size, 0, -1):
     team_list.append(int(i))
-print(team_list)
-
-
-
-print("iteration", " ==> ", "new_list")
+print("team list :", team_list)
 
 while iteration < team_size:
     repetition = team_size - iteration
     new_list = [iteration] * repetition
 
-    #print(iteration)
-    #print(team_size, iteration, repetition)
-    #print("iteration, repetition :", iteration, repetition, " ==> ", "new_list :", new_list)
-
-    #print(len(new_list), "==>", new_list)
-    #print(new_list)
-    print(iteration, " ==> ", new_list)
-
     total_list_count += len(new_list)
-    #print(total_list_count)
-
     total_list += new_list
-
     iteration += 1
-#print(" ")
-#print("total_list :", total_list)
+print("total_list :", total_list)
+
+# create a GV file from the total_list in current directory
+fp = open('input.gv', 'w')
+
+# Opening definition and bracket of file
+fp.write('graph MyGraph {')
+fp.write('\n')
+
+# Content from total_list
+for items in total_list:
+    fp.writelines(str(items) + " -- " + "2" +'\n')
+
+# replace 2 with connecting teamnumber thingy
+
+"""" Idea
+for each list_entry iterate through the team_list entries
+Check if list_entry is greater or equal with the team_list entry, then skip
+
+for each... iteration durch new_list!
+    if new_list_item > team_list
+        doch nicht reversed!!
+        1, 1, 2      =<   1, 2, 3
+        fp.writelines(str(items) + " -- " + team_list_item" +'\n') 
+        
+        teamsize 3
+        team list 1,2,3
+        new_list 1,1,2
+        
+        1 => 2
+        1 => 3
+        2 => 3
+"""
+
+
+# End of content from total_list
+
+# Closing bracket in last line of file
+fp.write('\n')
+fp.write('}')
+fp.close()
+
+
+# Execute the DOT command in the terminal to create the dot graph image "output.png"
+import subprocess
+
+bashCommand = "dot -Tpng input.gv -o output.png"
+process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
